4841895ff04daa6d5016e1d07ffc6a19358c22bf
Jeanne COLMOU <jecolmou@made-f0Dr11s10.clusters.42paris.fr>
2023-08-25 15:57:52 +0200

    pointeurs sur fonctions por command 2

--- Command.cpp
+++ Command.cpp
@@ -12,43 +12,22 @@ Command::~Command()
 
 Command::Command(std::string com, std::string leftover, Client *my_client)
 {
-	// std::vector<
 	(void)leftover;
 
 	Server *tmp = my_client->getMyServer();
-	// if (tmp == NU:)
-
-    // std::cout << "AFTER ADDING THE SERVER = " << my_client->getMyServer()->GetServerName() << std::endl;
-	// exit(1);
 	std::vector<std::string> tmp_compTab = tmp->GetComptab();
-	std::vector<Command::fct*> tmp_functionTab = tmp->GetFunctionTab();
-	// std::cout << "Commande ----> " << com << " | Reste = " << leftover << std::endl;
-		int j(0);
+	std::vector<void (Command::*)()> tmp_functionTab = tmp->GetFunctionTab();
+	int j(0);
 	for (std::vector<std::string>::iterator it = tmp_compTab.begin(); it != tmp_compTab.end(); it++)
 	{
-        // std::cout << "tab i = " << compTab[i] << " | level = " << com << std::endl;
 		if (*it == com)
 		{
-			std::cout << "it => " << *it << std::endl;
-			std::cout << "com => " << com << std::endl;
-			std::cout << "j => " << j << std::endl;
 			tmp_functionTab[j];
-			(this->*(*it))();
+			(this->*tmp_functionTab[j])();
 		}
 		j++;
 	}
 }
-
-//  size_t index = std::distance(tmp_compTab.begin(), it);
-
-//         // Vérifier que l'indice est valide
-//         if (index < tmp_functionTab.size()) {
-//             // Appeler la fonction pointée par l'indice
-//             Server::fct func = tmp_functionTab[index];
-//             (my_server->*func)(); // Appel de la fonction
-//         } else {
-//             std::cerr << "Indice en dehors de la plage du vecteur tmp_functionTab." << std::endl;
-//         }
 
 void Command::capls()
 {
