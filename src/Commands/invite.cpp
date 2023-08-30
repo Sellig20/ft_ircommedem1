b938fd3d6b0a37c978f8783d5ac77d89cc79cecb
ffougear <82102851+FannyCaulfield@users.noreply.github.com>
2023-08-30 21:09:40 +0000

    merge avec bonne archi de dossier

---
+++ src/Commands/invite.cpp
@@ -0,0 +1,7 @@
+#include "../includes/Command.hpp"
+
+void Command::invite()
+{
+	std::cout << "++++++++++++ je suis dans invite  voici le leftorvers : " << command_leftovers << "+++++++++++++" << std::endl;
+}
+
---
+++ src/Commands/oper.cpp
@@ -0,0 +1,8 @@
+#include "../includes/Command.hpp"
+
+void Command::oper()
+{
+	std::cout << "++++++++++++ je suis dans oper voici le leftorvers : " << command_leftovers << "+++++++++++++" << std::endl;
+
+}
+
